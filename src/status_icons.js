4e84f89faba75eac786627f8a1005172048b9c43
Alex Yu <alex.yu.dev@gmail.com>
2025-08-09 01:50:36 +0300

    chore: add resources

---
+++ src/status_icons.js
@@ -0,0 +1,89 @@
+// http status icons codes
+
+// status icons
+module.exports = {
+  defaults: {
+    '4xx': 'emo-confusion',
+    '5xx': 'emo-dead',
+  },
+
+  // 1xx Інформаційні
+
+  100: '',
+  101: '',
+  102: '',
+  103: '',
+
+  // 2xx Успіх
+
+  200: '',
+  201: '',
+  202: '',
+  203: '',
+  204: '',
+  205: '',
+  206: '',
+  207: '',
+  208: '',
+  226: '',
+
+  // 3xx Перенаправлення
+
+  300: '',
+  301: '',
+  302: '',
+  303: '',
+  304: '',
+  305: '',
+  306: '',
+  307: '',
+  308: '',
+
+  // 4xx Помилка клієнта
+
+  400: 'emo-confusion',
+  401: 'fawkes-mask',
+  402: 'coin',
+  403: 'hand-stop',
+  404: 'emo-glass',
+  405: 'hand-stop', // cross
+  406: 'hand-stop',
+  407: 'mask', // eye-hand, mask, jack-o-lantern
+  408: 'time-cycling',
+  409: 'collision',
+  410: 'emo-hiding',
+  411: 'emo-sad',
+  412: 'emo-sad',
+  413: 'emo-sad',
+  414: 'emo-sad',
+  415: 'emo-sad',
+  416: 'emo-sad',
+  417: 'emo-sad',
+  418: 'teapot-1',
+  421: 'wall',
+  422: 'emo-sad',
+  423: 'locked-shield', // locked, locked-shield, emo-silence
+  424: 'emo-sad',
+  425: 'emo-sad',
+  426: 'emo-sad',
+  427: 'emo-sad',
+  428: 'emo-sad',
+  429: 'emo-bandage',
+  430: 'emo-sad',
+  431: 'emo-bandage',
+  451: 'emo-sad',
+
+  // 5xx Помилка сервера
+  500: 'emo-dead',
+  501: 'emo-dead',
+  502: 'emo-dead',
+  503: 'emo-dead',
+  504: 'time-cycling',
+  505: 'emo-dead',
+  506: 'emo-dead',
+  507: 'emo-dead',
+  508: 'emo-dead',
+  509: 'emo-dead',
+  510: 'emo-dead',
+  511: 'emo-dead',
+};
